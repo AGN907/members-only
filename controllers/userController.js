35f26484207c9d831eddda34b9ec084d9424173d
AGN <itzme907@gmail.com>
2023-10-20 21:06:12 +0300

    feat: add signup validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const User = require('../models/user')
 const bycrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
-
+const { body, validationResult } = require('express-validator')
 
 exports.signup_get = asyncHandler(async (req, res, next) => {
   res.render('signup_form', {
@@ -10,7 +10,45 @@ exports.signup_get = asyncHandler(async (req, res, next) => {
   })
 })
 
-exports.signup_post = asyncHandler(async (req, res, next) => {
+exports.signup_post = [
+  
+  body('firstName', 'First name can\'t be empty')
+    .trim()
+    .isLength({min:1})
+    .escape(),
+  body('lastName', 'Last name can\'t be empty')
+    .trim()
+    .isLength({min:1})
+    .escape(),
+  body('email', 'Email can\' be empty')
+    .trim()
+    .isLength({min:1})
+    .isEmail()
+    .withMessage('Please enter a valid email eg: example@example.com')
+    .escape(),
+  body('password', 'Password can\t be empty')
+    .trim()
+    .isLength({min:1})
+    .escape(),
+  body('confirm-password')
+    .trim()
+    .custom((value, {req}) => {
+    if (value !== req.body.password) {
+      throw new Error('Passwords don\'t match!')
+    }
+    return value === req.body.password
+  }).escape(),
+  
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+    
+    if (!errors.isEmpty()) {
+      res.render('signup_form', {
+        errors: errors.array()
+    })
+    return
+  }
+
   bycrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
     if (err) {
       return next(err)
@@ -20,9 +58,14 @@ exports.signup_post = asyncHandler(async (req, res, next) => {
       lastName: req.body.lastName,
       email: req.body.email,
       password: hashedPassword,
+      memberStatus: 'non-member'
     })
     await user.save()
     res.redirect('/')
   })
-})
+
+  
+})]
+
+
 
