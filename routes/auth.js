82b52cfca20ab109f8d9aa840e19cec4ade16aa0
AGN <itzme907@gmail.com>
2023-10-20 22:47:45 +0300

    feat: add users login

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,20 @@
 const router = require('express').Router()
+const passport = require('passport')
 const userController = require('../controllers/userController')
 
 
 router.get('/sign-up', userController.signup_get)
-
-
 router.post('/sign-up', userController.signup_post)
+
+router.get('/log-in', userController.login_get)
+router.post(
+  "/log-in",
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/log-in'
+    })
+)
+ 
 
 
 module.exports = router
